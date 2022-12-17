bba6bca9d3a6b08b42dd137daab5eed02b36e151
paradox20029 <93834245+paradox20029@users.noreply.github.com>
2022-12-17 18:59:24 +0530

    Update display.py

--- Hospital_management-python_tkinter-master/display.py
+++ Hospital_management-python_tkinter-master/display.py
@@ -1,29 +1,32 @@
-from tkinter import *
+#armaan 2110994755
+from tkinter import * #importing librariries for using sql and images 
 import sqlite3
 import pyttsx3
 from PIL import ImageTk,Image
-conn = sqlite3.connect('database1.db')
+conn = sqlite3.connect('database1.db') #establishing connection with the data base using .connect method
 c = conn.cursor()
 
 number = []
 patients = []
 
-sql = "SELECT * FROM appointment"
-res = c.execute(sql)
+sql = "SELECT * FROM appointment" #running the sql query with command select
+res = c.execute(sql) #using execute method to execute the sql query
 for r in res:
     ids = r[0]
     name = r[1]
     number.append(ids)
     patients.append(name)
 
-class Application:
-    def __init__(self, master):
+class Application: #application class created
+    def __init__(self, master): #"__init__" is a reseved method in python classes.
+        #It is known as a constructor in object oriented concepts. 
+        #This method called when an object is created from the class and it allow the class to initialize the attributes of a class.
         self.master = master
-        self.image=ImageTk.PhotoImage(Image.open("hospital6.jpg"))
+        self.image=ImageTk.PhotoImage(Image.open("hospital6.jpg")) #using the image for display
         self.panel=Label(master,image=self.image)
         self.panel.pack()
         self.x = 0
-
+#using the widgtes such as label and button to create the display
         self.heading = Label(master, text="Appointments", font=('arial 60 bold'), fg='green')
         self.heading.place(x=150, y=0)
 
