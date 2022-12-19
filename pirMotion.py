e23bda4940e3eeef2fd19dad1403a3ba9eb3ce80
paradox20029 <93834245+paradox20029@users.noreply.github.com>
2022-12-19 13:13:41 +0530

    Update pirMotion.py

--- pirMotion.py
+++ pirMotion.py
@@ -1,11 +1,11 @@
-from gpiozero import MotionSensor
+from gpiozero import MotionSensor #importing the motion sensor libraries and led
 from gpiozero import LED
-led=LED(17)
+led=LED(17) #defining the GPIO pin for the led and motion sensor
 pir = MotionSensor(26)
 
 while True:
-	pir.wait_for_motion()
-	print("patient detected")
+    pir.wait_for_motion() #using the inbuilt method to intiate the sensor when the person comes in the range
+    print("patient detected") #printing message in accordance to the detection
     led.on()
     pir.wait_for_no_motion()
     print("patient not detected")
